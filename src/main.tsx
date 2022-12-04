fa21e8c5e7e7814e1fd494010d9754da97350220
carloscmarcelino <carloslima1598@gmail.com>
2022-12-04 10:46:07 -0300

    configure chakra theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: '#000',
+      },
+    }),
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
