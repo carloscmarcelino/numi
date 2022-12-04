299fbda2dcd9c18876123bc7ac82f857f02d21c8
carloscmarcelino <carloslima1598@gmail.com>
2022-12-04 10:46:32 -0300

    added calculator

---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,24 @@
+import { Textarea } from '@chakra-ui/react';
+
+type CalculatorProps = {
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  value: string;
+};
+
+export const Calculator: React.FC<CalculatorProps> = ({ value, setValue }) => {
+  return (
+    <Textarea
+      border="none"
+      outline="none"
+      boxSize="border-box"
+      bg="#282a2d"
+      color="#e3e7e9"
+      fontSize="1.25rem"
+      resize="none"
+      _focus={{ outline: 'none' }}
+      overflow="hidden"
+      value={value}
+      onChange={({ currentTarget: { value } }) => setValue(value)}
+    />
+  );
+};
