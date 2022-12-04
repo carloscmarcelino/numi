6fe2044dfd11753ace6c1edb66f1f9ae2b827325
carloscmarcelino <carloslima1598@gmail.com>
2022-12-04 10:46:53 -0300

    created the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Grid } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Calculator, Result } from './components';
 
 function App() {
-  return <Box>App</Box>;
+  const [value, setValue] = useState<string>('');
+  const [result, setResult] = useState<(number | null)[]>();
+
+  const evaluate = (value: string): number | null => {
+    try {
+      if (value.match(/[a-zA-Z&#$<>{}]/g)) throw new Error();
+
+      return new Function(`return (${value})`)();
+    } catch (error) {
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const lines = value.split(/\r?\n/).map(evaluate);
+
+    setResult(lines);
+  }, [value]);
+
+  return (
+    <Grid
+      gridTemplateColumns="1.5fr 0.2fr"
+      bg="#282a2d"
+      maxW="800px"
+      h="400px"
+      m="auto"
+    >
+      <Calculator value={value} setValue={setValue} />
+
+      <Result result={result} />
+    </Grid>
+  );
 }
 
 export default App;
